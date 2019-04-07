d3f52e0064cd75a641796284ca507e58314ebb61
sullylei <sullylei0207@gmail.com>
2019-04-07 22:55:24 +0800

    黑马的vue视频演示

---
+++ constants/test.constants.js
@@ -0,0 +1,13 @@
+const  stateArr = {
+    '1':'待审批',
+    '2':'审批通过',
+    '3':'状态未知'
+}
+
+function getStateName(state , stateArr){
+    for(int i =0;i<stateArr;i++){
+
+    }
+}
+
+export const getStateName = state => stateArr[state]
